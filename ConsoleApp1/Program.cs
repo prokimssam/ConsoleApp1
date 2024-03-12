5a58a2ca54e29a98cd95dec67a54ce2e68e2296a
prokimssam <86967542+prokimssam@users.noreply.github.com>
2024-03-12 11:17:45 +0900

    GiftBox 추가
    Letter와 Moneny를 넣고, 배송기사님도 넣어줬다.
    그리고 public도 써보고
    여려줄 그냥 쓰고 싶은 말을 써 보세요.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace ConsoleApp1
+﻿using System.Xml;
+
+namespace ConsoleApp1
 {
+    class GiftBox
+    {
+        public string Letter;
+        public int Money;
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GiftBox a = new GiftBox()
+            {
+                Letter = "잘 지내지?",
+                Money = 100000
+            };
+
+            GiftBox 배송기사님 = a;
+
+            a.Letter = "어떻게 지내고 있어?";
+            a.Money = 150000;
         }
     }
 }
+
