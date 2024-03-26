3fb8c4575b0bd3d6e4673fa5a4b42f3cb2dda2cd
prokimssam <86967542+prokimssam@users.noreply.github.com>
2024-03-26 11:23:37 +0900

    2주 Array, List

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿
+
 
 namespace ConsoleApp1
 {
@@ -13,19 +14,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            GiftBox a = new GiftBox()
+            GiftBox addressA = GiftBoxMaker("A친구야 잘 지내지?", 110000);
+            GiftBox addressB = GiftBoxMaker("B친구야 잘 지내지?", 120000);
+            GiftBox addressC = GiftBoxMaker("C친구야 잘 지내지?", 130000);
+
+            GiftBox[] giftBoxes = new GiftBox[3];
+            giftBoxes[0] = addressA;
+            giftBoxes[1] = addressB;
+            giftBoxes[2] = addressC;
+            
+            Console.WriteLine("giftBox Array Count : " + giftBoxes.Length);
+
+            //List~
+            List<GiftBox> giftBoxList = new List<GiftBox>();
+            giftBoxList.Add(addressA); //0
+            giftBoxList.Add(addressB); //1
+            giftBoxList.Add(addressC); //2
+
+            Console.WriteLine(giftBoxList[2].Letter);
+            Console.WriteLine("giftbox List Count : " + giftBoxList.Count);
+        }
+
+        private static string add(int v1, int v2)
+        {
+            int num = v1 + v2;
+            return num + "입니다.";
+        }
+
+        private static GiftBox GiftBoxMaker(string letter, int money)
+        {
+            GiftBox addressA = new GiftBox()
             {
-                Letter = "잘 지내지?",
-                Money = 100000
+                Letter = letter,
+                Money = money
             };
 
-            GiftBox 배송기사님 = a;
-
-            a.Letter = "어떻게 지내고 있어?";
-            a.Money = 150000;
-
-            Console.WriteLine("bbbbbbbbbbbbbb");
-            Console.WriteLine("cccccc22222");
+            return addressA;
         }
     }
 }
