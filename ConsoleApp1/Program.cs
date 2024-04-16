3f2f6a26ec24cd2eef4332ef7d7a3e570357aef3
prokimssam <86967542+prokimssam@users.noreply.github.com>
2024-04-16 09:31:50 +0900

    ValueType, Reference Type

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,10 +5,24 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("시작 부분");
 
-            GiftBoxManager manager = new GiftBoxManager();
-            manager.SetInit();
+            int aMoney = 10000;
+            int bMoney = aMoney;
+            aMoney = 20000;
+            Console.WriteLine("aMoney : " + aMoney);
+            Console.WriteLine("bMoney : " + bMoney);
 
+            GiftBox aGiftBox = new GiftBox()
+            {
+                Letter = "안녕",
+                Money = 10000
+            };
+
+            GiftBox bGiftBox = aGiftBox;
+            aGiftBox.Money = 20000;
+            Console.WriteLine("aGiftBox : " + aGiftBox.Money);
+            Console.WriteLine("bGiftBox : " + bGiftBox.Money);
         }
     }
 }
